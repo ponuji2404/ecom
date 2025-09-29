b44c201f93b1e6eff1299a7abcc0595409d3439b
ponuji2404 <81793086+ponuji2404@users.noreply.github.com>
2025-09-29 10:48:12 +0530

    Add files via upload

---
+++ customer/pay_offline.php
@@ -0,0 +1,53 @@
+<center><!-- center Starts -->
+
+<h1> Pay OffLine Using Method  </h1>
+
+<p class="text-muted" >
+
+If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+
+</p>
+
+</center><!-- center Ends -->
+
+<hr>
+
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th> Bank Account Details </th>
+
+<th> UBL Omni,Mobi Cash Details: </th>
+
+<th> Western Union Details: </th>
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!-- tbody Starts -->
+
+<tr>
+
+<td> Bank Name:ABC Account No:03333333 Branch Code:0342 Branch Name:INDIA	 </td>
+
+<td> NIC#001230006 Mobile No:1213014150, Name:DemoName </td>
+
+<td> Full Name:Demo Name, Mobile No:1213014150, Name:Demo, Country:US, N.I.C No:011222334
+</td>
+
+
+</tr>
+
+</tbody><!-- tbody Ends -->
+
+
+</table><!-- table table-bordered table-hover table-striped Ends -->
+
+</div><!-- table-responsive Ends -->
---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+
+session_start();
+
+include("includes/db.php");
+include("includes/header.php");
+include("functions/functions.php");
+include("includes/main.php");
+
+?>
+
+
+  <!-- Cover -->
+  <main>
+    <div class="hero">
+      <a href="shop.php" class="btn1">View all products
+</a>
+    </div>
+    <!-- Main -->
+    <div class="wrapper">
+            <h1>Featured Collection<h1>
+            
+      </div>
+
+
+
+    <div id="content" class="container"><!-- container Starts -->
+
+    <div class="row"><!-- row Starts -->
+
+    <?php
+
+    getPro();
+
+    ?>
+
+    </div><!-- row Ends -->
+
+    </div><!-- container Ends -->
+    <!-- FOOTER -->
+    <footer class="page-footer">
+
+      <div class="footer-nav">
+        <div class="container clearfix">
+
+          <div class="footer-nav__col footer-nav__col--info">
+            <div class="logo">
+              <a class="logo__link" href="index.php">
+                <img class="logo__img" src="images/logo.png" alt="Avenue fashion logotype" width="237" height="19">
+              </a>
+            </div>
+            <br>
+            <div class="footer-nav__heading"></div>
+            <ul class="footer-nav__list">
+              <li class="footer-nav__item">
+                <a href="#" class="footer-nav__link">Welcome to mini_Bazar! We founded this business after realizing the lack of localized e-Shops. <br>
+                  Now, we're dedicated to localized the e-com by providing Products that are best in Quality & Price.</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="footer-nav__col footer-nav__col--contacts">
+            <div class="footer-nav__heading">Contact details</div>
+            <address class="address">
+            Head Office: IGNOU, Naraina Vihar,
+            <br>
+            New Delhi-110028, Delhi, India.
+          </address>
+            <div class="phone">
+              Telephone:
+              <a class="phone__number" href="tel:0123456789">+91-1234511111</a>
+            </div>
+            <div class="email">
+              Email:
+              <a href="mailto:support@minibazarxyz.com" class="email__addr">support@minibazarxyz.com</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="banners">
+        <div class="container clearfix">
+
+          <div class="banner-social">
+            <a href="#" class="banner-social__link">
+            <i class="icon-facebook"></i>
+          </a>
+            <a href="#" class="banner-social__link">
+            <i class="icon-twitter"></i>
+          </a>
+            <a href="#" class="banner-social__link">
+            <i class="icon-instagram"></i>
+          </a>
+            <a href="#" class="banner-social__link">
+            <i class="icon-pinterest-circled"></i>
+          </a>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="page-footer__subline">
+        <div class="container clearfix">
+
+          <div class="copyright">
+            &copy; <?php echo date("Y");?> Ecommerce Website IGNOU&trade;
+          </div>
+
+          <div class="developer">
+            Developed by Pranav
+          </div>
+
+        </div>
+      </div>
+    </footer>
+</body>
+
+</html>
